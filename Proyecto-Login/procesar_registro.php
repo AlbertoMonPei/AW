7de081ac782b76eba8f0cd8923c8606c74418960
AlbertoMonPei <amp268@educa.madrid.org>
2025-11-07 10:35:49 -0500

    Eliminación fondos y modificacion de estilos

--- Proyecto-Login/procesar_registro.php
+++ Proyecto-Login/procesar_registro.php
@@ -7,6 +7,20 @@ $file=fopen("usuarios.txt","a");
 fwrite($file, $usuario. ":".password_hash($password, PASSWORD_DEFAULT). "\n");
 fclose($file);
 
-echo"<h1>Usuario registrado correctamente</h1>";
-echo"<p><a href='login.php'>Iniciar sesión</a></p>";
 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro exitoso</title>
+</head>
+<body>
+    <div class="contenedor-mensaje">
+        <h1>¡Usuario registrado correctamente!</h1>
+        <a href="login.php" class="boton-login">Iniciar sesión</a>
+    </div>
+</body>
+</html>
