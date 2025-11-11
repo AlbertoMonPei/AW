206ff73fa7c60b156a2e536f6bb6198b53c4b205
AlbertoMonPei <amp268@educa.madrid.org>
2025-11-11 13:57:11 -0500

    editar/organizar

--- Proyecto-Login/login.php
+++ Proyecto-Login/login.php
@@ -31,105 +31,21 @@ else if (isset($_SESSION['registro_exitoso'])) {
     <title>Iniciar sesión</title>
     <link rel="stylesheet" href="CSS/estilos.css"> 
 
-    <style>
-        .modal-overlay {
-            position: fixed; /* Se queda fijo en la pantalla */
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            background-color: rgba(0, 0, 0, 0.6);
-            
-            /* Centra la ventana */
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            
-            /* Oculto por defecto */
-            opacity: 0;
-            visibility: hidden;
-            transition: opacity 0.3s ease;
-            z-index: 1000; /* Asegura que esté por encima de todo */
-        }
-
-        /* Clase para MOSTRAR el modal */
-        .modal-overlay.show {
-            opacity: 1;
-            visibility: visible;
-        }
-
-        /* Estilo ventana */
-        .modal-content {
-            background-image: 
-        /* Capa 1: Un velo oscuro semitransparente */
-        linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)),
-        
-        /* Capa 2: Imagen de fondo */
-        url('Imagenes/ventana-error-exito.jpg');
-            background-size: cover;
-            background-position: center;
-            color: white;
-            padding: 20px;
-            border-radius: 8px;
-            box-shadow: 0 4px 10px rgba(255, 255, 255, 0.3);
-            max-width: 400px;
-            width: 90%;
-            box-sizing: border-box;
-            text-align: center;
-            color: #333;
-        }
-        /*Estilo titulo menaje*/
-        .modal-content h3 {
-            margin-top: 0;
-            margin-bottom: 15px;
-            font-size: 1.1rem;
-            color: #ffffffff;
-        }
-        /*Estilo texto mensaje*/
-        .modal-content p {
-            color: #ffffffff;
-            margin-top: 0;
-            margin-bottom: 25px;
-            font-size: 1rem;
-        }
-
-        /* Contenedor para alinear el botón en el centro */
-        .modal-actions {
-            text-align: center;
-        }
-
-        /* Botón "OK" */
-        #modal-close-btn {
-            background-color: #ff00c8;
-            color: white;
-            font-size: 0.9rem;
-            font-weight: bold;
-            border: none;
-            padding: 10px 20px;
-            border-radius: 5px;
-            cursor: pointer;
-            transition: background-color 0.2s ease;
-        }
-
-        #modal-close-btn:hover {
-            background-color: #0056b3;
-        }
-    </style>
 </head>
+
 <body>
 
+    <h1>Login</h1>
     <form action="procesar_login.php" method="post">
-        <h1>Login</h1>
-        
-        <label>Usuario:</label>
-        <input type="text" name="usuario" required>
+    <label>Usuario:</label>
+    <input type="text" name="usuario" required>
 
-        <label>Contraseña:</label>
-        <input type="password" name="password" required>
+    <label>Contraseña:</label>
+    <input type="password" name="password" required>
         
-        <button type="submit">Entrar</button>
+    <button type="submit">Entrar</button>
 
-        <p>¿No tienes cuenta? <a href="registro.php">Regístrate aquí</a></p>
+    <p>¿No tienes cuenta? <a href="registro.php">Regístrate aquí</a></p>
     </form>
 
 
--- Proyecto-Login/registro.php
+++ Proyecto-Login/registro.php
@@ -5,17 +5,22 @@
 <title>Registro de usuario</title>
 <link rel="stylesheet" href="CSS/estilos.css">
 </head>
+
 <body>
 
-    <h1>REGISTRO</h1>
+    <h1>Registro</h1>
     <form action="procesar_registro.php" method="post">
     <label>Usuario:</label>
     <input type="text" name="usuario" required><br><br>
+    
     <label>Contraseña:</label>
     <input type="password" name="password" required><br><br>
+    
     <button type="submit">Registrarse</button>
-    </form>
+
     <p>¿Ya tienes cuenta? <a href="login.php">Inicia sesión aquí</a></p>
+    </form>
+    
 
 
 
