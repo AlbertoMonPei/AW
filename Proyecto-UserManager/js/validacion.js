d5a0a6d3d0f9b78b2a6685b672d0cbc6ec85a276
AlbertoMonPei <amp268@educa.madrid.org>
2026-01-14 14:53:48 -0500

    quito comentarios para prueba

--- Proyecto-UserManager/js/validacion.js
+++ Proyecto-UserManager/js/validacion.js
@@ -14,23 +14,23 @@ document.addEventListener("submit", function(event){
 
         if(nombre.length <3){
             errores.push("El nombre debe tener al menos 3 caracteres.");
-        } //Comprueba que el dato tiene más de 3 caracteres
+        }
 
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         if(!emailRegex.test(email)){
             errores.push("Introduce un email válido")
-        } //comprueba que se sigue el formato correo @
+        }
         
         if(isNaN(edad) || edad <1 || edad >120){
             errores.push("La edad debe ser un número entre 1 y 120")
-        } //comprueba que la edad NO es menor que 1 ni mayor que 120
+        }
         
         if(rol !=="user" && rol!=="admin"){
             errores.push("Selecciona un rol válido.");
-        } //comprueba que se ha seleccionado un rol, sino devuelve un error
+        }
 
         if(errores.length > 0){
-            event.preventDefault(); //Evita que se envíe el formulario
+            event.preventDefault();
             alert("Por favor, corrige los siguientes errores:\n" + errores.join("\n"));
         }
 
